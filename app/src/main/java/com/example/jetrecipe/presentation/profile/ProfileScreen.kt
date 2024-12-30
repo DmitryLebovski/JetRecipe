4f01240b273fad811fe95be3aeac073b6b5f52eb
DmitryLebovski <shedik.31@mail.ru>
2024-12-30 16:43:40 +0300

    feat: BottomBar

--- app/src/main/java/com/example/jetrecipe/presentation/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/jetrecipe/presentation/profile/ProfileScreen.kt
@@ -1,6 +1,5 @@
 package com.example.jetrecipe.presentation.profile
 
-import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
@@ -11,15 +10,20 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
+import com.example.jetrecipe.R
 import com.example.jetrecipe.domain.model.User
 
 @Composable
@@ -45,20 +49,30 @@ fun ProfileScreen(
             )
             Spacer(modifier = Modifier.height(16.dp))
 
-            Text(text = "Имя: ${it.username}", style = MaterialTheme.typography.bodyMedium)
-            Text(text = "Email: ${it.email}", style = MaterialTheme.typography.bodyMedium)
+            Text(
+                text = "Имя: ${it.username}",
+                style = MaterialTheme.typography.bodyMedium,
+                fontFamily = FontFamily(Font(R.font.inter)
+            ))
+            Text(
+                text = "Email: ${it.email}",
+                style = MaterialTheme.typography.bodyMedium,
+                fontFamily = FontFamily(Font(R.font.inter))
+            )
             Spacer(modifier = Modifier.height(8.dp))
+
         } ?: run {
             Text(text = "Данные о пользователе отсутствуют")
             Spacer(modifier = Modifier.height(8.dp))
         }
 
         Button(onClick = {
-            Log.d("ProfileScreen", "Logout")
             Toast.makeText(context, "Выйти", Toast.LENGTH_SHORT).show()
             onSignOut()
-        }) {
-            Text(text = "Sign out")
+        },
+            colors = ButtonDefaults.buttonColors(Color.Red)
+        ) {
+            Text(text = "Выйти из аккаунта")
         }
     }
 }
---
+++ app/src/main/java/com/example/jetrecipe/tools/BottomBar.kt
@@ -0,0 +1,31 @@
+package com.example.jetrecipe.tools
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.example.jetrecipe.utils.Routes.MAIN_SCREEN
+
+@Composable
+fun BottomBar(navController: NavController) {
+    NavigationBar {
+        NavigationBarItem(
+            icon = { Icon(Icons.Default.Home, contentDescription = "Home") },
+            selected = navController.currentDestination?.route == MAIN_SCREEN,
+            onClick = { navController.navigate(MAIN_SCREEN) }
+        )
+        NavigationBarItem(
+            icon = { Icon(Icons.Default.Search, contentDescription = "Settings") },
+            label = { Text("Поиск") },
+            selected = false,
+            enabled = false,
+            onClick = { }
+        )
+    }
+}
