51b91f6fc03267e706ba39271045122e1af39d1e
DmitryLebovski <shedik.31@mail.ru>
2024-12-30 16:43:24 +0300

    fix:sign out

--- app/src/main/java/com/example/jetrecipe/MainActivity.kt
+++ app/src/main/java/com/example/jetrecipe/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.navigation.compose.rememberNavController
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.mutableStateOf
@@ -14,9 +16,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.jetrecipe.domain.model.User
 import com.example.jetrecipe.presentation.login.LoginScreen
 import com.example.jetrecipe.presentation.main_screen.MainScreen
+import com.example.jetrecipe.tools.BottomBar
 import com.example.jetrecipe.ui.theme.JetRecipeTheme
 import com.example.jetrecipe.utils.Routes.LOGIN_SCREEN
 import com.example.jetrecipe.utils.Routes.MAIN_SCREEN
@@ -52,33 +56,43 @@ class MainActivity : ComponentActivity() {
                         .fillMaxSize()
                 ) {
                     val navController = rememberNavController()
+                    val currentBackStackEntry = navController.currentBackStackEntryAsState()
 
-                    NavHost(
-                        navController = navController,
-                        startDestination = startDestination
-                    ) {
-                        composable(LOGIN_SCREEN) {
-                            LoginScreen(
-                                onSignInSuccess = { newUser ->
-                                    userState.value = newUser
-                                    navController.navigate(MAIN_SCREEN) {
-                                        popUpTo(LOGIN_SCREEN) { inclusive = true }
-                                    }
-                                }
-                            )
+                    Scaffold (
+                        bottomBar = {
+                            if (currentBackStackEntry.value?.destination?.route == MAIN_SCREEN) {
+                                BottomBar(navController)
+                            }
                         }
-                        composable(MAIN_SCREEN) {
-                            MainScreen(
-                                navController = navController,
-                                user = userState.value,
-                                onSignOut = {
-                                    Firebase.auth.signOut()
-                                    userState.value = null
-                                    navController.navigate(LOGIN_SCREEN) {
-                                        popUpTo(MAIN_SCREEN) { inclusive = true }
+                    ){ paddingValues ->
+                        NavHost(
+                            navController = navController,
+                            startDestination = startDestination,
+                            modifier = Modifier.padding(paddingValues)
+                        ) {
+                            composable(LOGIN_SCREEN) {
+                                LoginScreen(
+                                    onSignInSuccess = { newUser ->
+                                        userState.value = newUser
+                                        navController.navigate(MAIN_SCREEN) {
+                                            popUpTo(LOGIN_SCREEN) { inclusive = true }
+                                        }
                                     }
-                                }
-                            )
+                                )
+                            }
+                            composable(MAIN_SCREEN) {
+                                MainScreen(
+                                    navController = navController,
+                                    user = userState.value,
+                                    onSignOut = {
+                                        Firebase.auth.signOut()
+                                        userState.value = null
+                                        navController.navigate(LOGIN_SCREEN) {
+                                            popUpTo(MAIN_SCREEN) { inclusive = true }
+                                        }
+                                    }
+                                )
+                            }
                         }
                     }
                 }
